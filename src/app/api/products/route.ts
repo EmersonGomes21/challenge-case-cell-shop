b29022b45b8c8442576c16faafede8cd6ae8ff5f
Emerson Gomes <egr.softwares@gmail.com>
2026-06-05 02:26:47 -0300

    orderBy id

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -3,7 +3,7 @@ import prisma from '../../../lib/prisma'
 
 export async function GET() {
   try {
-    const products = await prisma.product.findMany()
+    const products = await prisma.product.findMany({orderBy: { id: 'asc' }})
     return NextResponse.json(products)
   } catch (e) {
     return NextResponse.json({ message: 'Erro interno' }, { status: 500 })
